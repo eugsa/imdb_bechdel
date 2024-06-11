a958090ce3ece5f93e114b34b9249e7ccf670ff3
eugsa <eugenie.saliou@gmail.com>
2024-06-11 10:12:16 +0200

    Clean comments

--- main.py
+++ main.py
@@ -18,13 +18,3 @@ def init():
 def generate_passing_per_year_plot():
   spark, df = init()
   visualization.generate_passing_per_year_plot(spark)
-  # passing_per_year_df = get_passing_per_year_df(spark).toPandas()
-  # plt.bar(passing_per_year_df.year, passing_per_year_df.movieCount)
-  # plt.title('Number of movies passing the Bechdel test per year')
-  # plt.xlabel('Years')
-  # plt.ylabel('Movie count')
-  # plt.show()
-
-
-
-# generate_passing_per_year_plot(spark)
